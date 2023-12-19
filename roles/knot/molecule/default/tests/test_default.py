bca2cf75959f342132e68b4a7bf6e1918907abd5
Bodo Schulz <bodo@boone-schulz.de>
2023-12-19 06:26:47 +0100

    add role knot

---
+++ roles/knot/molecule/default/tests/test_default.py
@@ -0,0 +1,116 @@
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.template import Templar
+import pytest
+import os
+import testinfra.utils.ansible_runner
+
+import pprint
+pp = pprint.PrettyPrinter()
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def base_directory():
+    cwd = os.getcwd()
+
+    if 'group_vars' in os.listdir(cwd):
+        directory = "../.."
+        molecule_directory = "."
+    else:
+        directory = "."
+        molecule_directory = "molecule/{}".format(os.environ.get('MOLECULE_SCENARIO_NAME'))
+
+    return directory, molecule_directory
+
+
+@pytest.fixture()
+def get_vars(host):
+    """
+
+    """
+    base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
+    operation_system = None
+
+    if distribution in ['debian', 'ubuntu']:
+        operation_system = "debian"
+    elif distribution in ['redhat', 'ol', 'centos', 'rocky', 'almalinux']:
+        operation_system = "redhat"
+    elif distribution in ['arch', 'artix']:
+        operation_system = f"{distribution}linux"
+
+    file_defaults = f"file={base_dir}/defaults/main.yaml name=role_defaults"
+    file_vars = f"file={base_dir}/vars/main.yaml name=role_vars"
+    file_molecule = f"file={molecule_dir}/group_vars/all/vars.yaml name=test_vars"
+    file_distibution = f"file={base_dir}/vars/{operation_system}.yaml name=role_distibution"
+
+    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars = host.ansible("include_vars", file_distibution).get("ansible_facts").get("role_distibution")
+    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+
+    ansible_vars = defaults_vars
+    ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
+    ansible_vars.update(molecule_vars)
+
+    templar = Templar(loader=DataLoader(), variables=ansible_vars)
+    result = templar.template(ansible_vars, fail_on_undefined=False)
+
+    return result
+
+
+def test_packages(host):
+    """
+    """
+    distribution = host.system_info.distribution
+    release = host.system_info.release
+
+    print(f"distribution: {distribution}")
+    print(f"release     : {release}")
+
+    packages = []
+    packages.append("knot")
+
+    # artix ist not supported
+    if not distribution == "artix":
+        for package in packages:
+            p = host.package(package)
+            assert p.is_installed
+
+
+@pytest.mark.parametrize("dirs", [
+    "/etc/knot",
+    "/var/lib/knot"
+])
+def test_directories(host, dirs):
+
+    d = host.file(dirs)
+    assert d.is_directory
+
+
+def test_service_running_and_enabled(host):
+    service = host.service('knot')
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_listening_socket(host, get_vars):
+    """
+    """
+    listening = host.socket.get_listening_sockets()
+
+    for i in listening:
+        print(i)
+
+    bind_address = "127.0.0.1"
+    bind_port = 5353
+
+    listen = []
+    listen.append(f"tcp://{bind_address}:{bind_port}")
+
+    for spec in listen:
+        socket = host.socket(spec)
+        assert socket.is_listening
