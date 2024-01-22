b42e25fb95542b3b4033f06d0a1d91b06f3649b3
Bodo Schulz <bodo@boone-schulz.de>
2024-01-22 12:08:57 +0100

    add role unbound

---
+++ roles/unbound/molecule/default/tests/test_default.py
@@ -0,0 +1,148 @@
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.template import Templar
+import pytest
+import os
+import testinfra.utils.ansible_runner
+
+import pprint
+pp = pprint.PrettyPrinter()
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def base_directory():
+    cwd = os.getcwd()
+
+    if ('group_vars' in os.listdir(cwd)):
+        directory = "../.."
+        molecule_directory = "."
+    else:
+        directory = "."
+        molecule_directory = "molecule/{}".format(os.environ.get('MOLECULE_SCENARIO_NAME'))
+
+    return directory, molecule_directory
+
+
+def read_ansible_yaml(file_name, role_name):
+    ext_arr = ["yml", "yaml"]
+
+    read_file = None
+
+    for e in ext_arr:
+        test_file = "{}.{}".format(file_name, e)
+        if os.path.isfile(test_file):
+            read_file = test_file
+            break
+
+    return "file={} name={}".format(read_file, role_name)
+
+
+@pytest.fixture()
+def get_vars(host):
+    """
+        parse ansible variables
+        - defaults/main.yml
+        - vars/main.yml
+        - vars/${DISTRIBUTION}.yaml
+        - molecule/${MOLECULE_SCENARIO_NAME}/group_vars/all/vars.yml
+    """
+    base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
+
+    print(" -> {}".format(distribution))
+    print(" -> {}".format(base_dir))
+
+    if distribution in ['debian', 'ubuntu']:
+        os = "debian"
+    elif distribution in ['redhat', 'ol', 'centos', 'rocky', 'almalinux']:
+        os = "redhat"
+    elif distribution in ['arch']:
+        os = "archlinux"
+
+    print(" -> {} / {}".format(distribution, os))
+
+    file_defaults = read_ansible_yaml("{}/defaults/main".format(base_dir), "role_defaults")
+    file_vars = read_ansible_yaml("{}/vars/main".format(base_dir), "role_vars")
+    file_distibution = read_ansible_yaml("{}/vars/{}".format(base_dir, os), "role_distibution")
+    file_molecule = read_ansible_yaml("{}/group_vars/all/vars".format(molecule_dir), "test_vars")
+    # file_host_molecule = read_ansible_yaml("{}/host_vars/{}/vars".format(base_dir, HOST), "host_vars")
+
+    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars = host.ansible("include_vars", file_distibution).get("ansible_facts").get("role_distibution")
+    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+    # host_vars          = host.ansible("include_vars", file_host_molecule).get("ansible_facts").get("host_vars")
+
+    ansible_vars = defaults_vars
+    ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
+    ansible_vars.update(molecule_vars)
+    # ansible_vars.update(host_vars)
+
+    templar = Templar(loader=DataLoader(), variables=ansible_vars)
+    result = templar.template(ansible_vars, fail_on_undefined=False)
+
+    return result
+
+
+@pytest.mark.parametrize("dirs", [
+    "/etc/unbound",
+    "/etc/unbound/unbound.conf.d",
+])
+def test_directories(host, dirs):
+    d = host.file(dirs)
+    assert d.is_directory
+    assert d.exists
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/unbound/unbound.conf",
+    "/etc/unbound/unbound.conf.d/server.conf",
+    "/etc/unbound/unbound.conf.d/forward_zone.conf",
+    "/etc/unbound/unbound.conf.d/remote_control.conf",
+    "/etc/unbound/unbound.conf.d/cache_db.conf",
+])
+def test_files(host, files):
+    f = host.file(files)
+    assert f.exists
+    assert f.is_file
+
+
+def test_user(host):
+    """
+      test service user
+    """
+    shell = "/usr/sbin/nologin"
+
+    distribution = host.system_info.distribution
+    release = host.system_info.release
+
+    if (distribution == 'debian' and release.startswith('9')):
+        shell = "/bin/false"
+
+    assert host.group("unbound").exists
+    assert host.user("unbound").exists
+    assert "unbound" in host.user("unbound").groups
+    assert host.user("unbound").shell == shell
+    assert host.user("unbound").home == "/var/lib/unbound"
+
+
+def test_service(host):
+    service = host.service("unbound")
+    assert service.is_enabled
+    assert service.is_running
+
+
+@pytest.mark.parametrize("ports", [
+    '0.0.0.0:53',
+    '127.0.0.1:8953',
+])
+def test_open_port(host, ports):
+
+    for i in host.socket.get_listening_sockets():
+        print(i)
+
+    application = host.socket("tcp://%s" % (ports))
+    assert application.is_listening
