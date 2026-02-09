b2453a1c211c8f2b04c1741a07b276a178a142a2
Bodo Schulz <bodo@boone-schulz.de>
2026-02-09 15:53:50 +0100

    updates for knot_resolver

---
+++ plugins/filter/knot.py
@@ -0,0 +1,47 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import absolute_import, division, print_function
+from typing import Any, List, Mapping, Sequence
+
+__metaclass__ = type
+
+from ansible.utils.display import Display
+
+# from ansible_collections.bodsch.dns.plugins.module_utils.network_type import reverse_dns
+
+# import json
+# import netaddr
+# import hashlib
+# import time
+# import re
+
+display = Display()
+
+# ---------------------------------------------------------------------------------------
+
+class FilterModule(object):
+    """ """
+
+    def filters(self):
+        return {
+            "knot_resolver_service": self.knot_resolver_service,
+        }
+
+    def knot_resolver_service(self, data: Sequence[str], os_family: str, count: int, service: str):
+        """ """
+        display.v(
+            f"knot_resolver_service(data: {data}, os_family: {os_family}, count: {count}, service: {service})"
+        )
+
+        _service: List = []
+
+        family = (os_family or "").strip().lower()
+
+        if family == "archlinux":
+            _service.append(data)
+        elif family == "debian":
+            for i in range(1, count+1):
+                _service.append(service.replace('@.', f"@{i}."))
+
+        display.v(f"  = {_service}")
+        return _service
+
