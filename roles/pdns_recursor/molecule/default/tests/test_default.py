e9a0c4147fcb8ef6a944fee97516c02dcf313c9c
Bodo Schulz <bodo@boone-schulz.de>
2026-05-28 06:15:11 +0200

    add update the APT cache task (#38)
    * add update the APT cache task
    * update bind molecule tests

--- roles/pdns_recursor/molecule/default/tests/test_default.py
+++ roles/pdns_recursor/molecule/default/tests/test_default.py
@@ -15,7 +15,7 @@ def test_directories(host, get_vars):
     """
     used config directory
     """
-    pp_json(get_vars)
+    print(get_vars)
 
     directories = [
         "/etc/powerdns/recursor.d",
